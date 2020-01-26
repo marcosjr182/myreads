67646f102eb44db97afcd4bf6925f558ac092d1c
Marcos Siqueira Junior <marcosjunior182@gmail.com>
2020-01-26 13:00:22 -0400

    refactor: Add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react'
-import './App.css'
+import { Route } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import SearchPage from './pages/SearchPage';
+import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-    books: [],
-  }
-
-  render() {
-    const { showSearchPage, books } = this.state;
-
-    return (
-      <div className="app">
-        {showSearchPage ? (<SearchPage />) : (<HomePage />)}
-      </div>
-    )
-  }
-
-}
+const BooksApp = () => (
+  <div className="app">
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/search' component={SearchPage} />
+  </div>
+)
 
 export default BooksApp
